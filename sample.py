68714795858f60f3830579dd8371cdd5fc57a89d
VICKY1991 <mohapatra.sambit8467@gmail.com>
2020-01-04 02:47:56 +0530

    sample commit vscode

--- sample.py
+++ sample.py
@@ -1,12 +1,17 @@
 import os
 import torch
 import numpy as np
-
+import matplotlib.pyplot as plt
+import cv2
 
 if __name__ == '__main__':
 
     print(torch.__version__)
     print(torch.cuda.is_available())
+
+    mat1 = np.array([1, 2, 3, 4])
+
+    print(mat1)
     pass
 
 
