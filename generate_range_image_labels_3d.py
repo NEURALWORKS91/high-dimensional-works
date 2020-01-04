a90c27818f4e0579865590540b40c1754de0a838
VICKY1991 <mohapatra.sambit8467@gmail.com>
2020-01-04 18:06:42 +0530

    sync

---
+++ generate_range_image_labels_3d.py
@@ -0,0 +1,22 @@
+import numpy as np
+import math
+import io
+import sys
+from simple_waymo_open_dataset_reader import WaymoDataFileReader
+from simple_waymo_open_dataset_reader import dataset_pb2, label_pb2
+from simple_waymo_open_dataset_reader import utils
+import cv2
+import matplotlib.pyplot as plt
+
+
+# file for the tfrecord data
+data_file_path = '/home/sambit/WAYMO/segment-1208303279778032257_1360_000_1380_000_with_camera_labels.tfrecord'
+
+if __name__ == "__main__":
+    print("============= generating training labels ===============")
+    print(np.__version__)
+
+    # read in the tfrecord data file
+    datafile = WaymoDataFileReader(data_file_path)
+
+    print(datafile)
